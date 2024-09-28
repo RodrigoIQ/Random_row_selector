e325a5b3147528dfa8e238bb19230a254d1d0b3d
RodrigoIQ <riq.dsing@gmaail.com>
2024-09-27 19:17:50 -0500

    added libary and path code

--- random_row_selector.py
+++ random_row_selector.py
@@ -3,4 +3,14 @@ import pandas as pd
 import os 
 import random
 from datetime import datetime
- 
+
+if __name__ == "__main__":
+    os.system('cls||clear')
+
+    print("Random Row Selector")
+    today = datetime.today()
+    print(today)
+
+    path = str(input("\n\nFile: "))
+    path = path.replace('"','')
+    print(path)
